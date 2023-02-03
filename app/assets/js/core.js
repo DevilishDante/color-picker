4532c457cadb07e823859e7a0762aec6d5542099
BuildTools <unconfigured@null.spigotmc.org>
2023-02-03 15:56:45 +0100

    link

--- app/assets/js/core.js
+++ app/assets/js/core.js
@@ -1,15 +1,9 @@
 
-function printColor(ev) {
-    const color = ev.target.value
+function printColor(color) {
     const r = parseInt(color.substr(1, 2), 16)
     const g = parseInt(color.substr(3, 2), 16)
     const b = parseInt(color.substr(5, 2), 16)
-    console.log(`red: ${r}, green: ${g}, blue: ${b}`)
-    console.log([r, g, b])
-    alert(`R: ${r}, G: ${g}, B: ${b}`)
+    const result_rgb = document.getElementById("result-rgb")
+    result_rgb.textContent = `R: ${r}, G: ${g}, B: ${b}`;
 }
-
-// document.getElementById('pick-color').parentElement.textContent = 'a'
-
-// document.getElementById('pick-color').addEventListener('click', printColor(ev), false);
-alert("oui")
+document.getElementById('pick-color').addEventListener('input',e => printColor(e.target.value))
