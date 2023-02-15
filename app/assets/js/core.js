9522181c0c4b3e0d18bac264f9e21e133749115c
BuildTools <unconfigured@null.spigotmc.org>
2023-02-15 16:09:05 +0100

    v1.1

--- app/assets/js/core.js
+++ app/assets/js/core.js
@@ -1,9 +1,10 @@
 // printcolor
-import {printColor} from './print-color.js'
+import {printColor,svg} from './print-color.js'
 // history
 import {saveColor,clearHistory,history} from './history.js'
 // event
 history()
+svg('#00000');
 document.getElementById('pick-color').addEventListener('input',e  => printColor(e.target.value))
 document.getElementById('pick-color').addEventListener('change',e  => printColor(e.target.value))
 document.getElementById('save-color').addEventListener('click',() => saveColor(document.getElementById('pick-color').value))
