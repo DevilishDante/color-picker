2453a6b43f4d4efcd32fadb8690d86e47d388777
BuildTools <unconfigured@null.spigotmc.org>
2023-02-16 08:42:54 +0100

    v1.1

--- BUGFIXES.md
+++
@@ -1,6 +0,0 @@
-# Paths v1.1
-
-* Code fully rewrited especialy for javascript part
-* An icon and tooltip added on interactions
-* Front part is better with any résolution
-* the bug about reverted sorting when colors was saved for the first time is solved
--- app/assets/js/history.js
+++ app/assets/js/history.js
@@ -1,6 +1,5 @@
 // affiche l'historique des couleurs
-import {tooltip,printColor} from './print-color.js'
-import {context} from './context-history.js'
+import {tooltip} from './print-color.js'
 export function history() {
     const history = document.getElementById("history")
     while (history.firstChild) history.removeChild(history.firstChild)
@@ -11,38 +10,23 @@ export function history() {
         history.appendChild(color)
     }
     if (!localStorage.getItem('colors')) return
-    let i = 0
-    colors.slice().reverse().forEach(function(e) {
+    let i = 9
+    colors.forEach(e => {
         const colored = history.querySelectorAll('span')[i]
         colored.style.backgroundColor = e
         colored.style.cursor = 'pointer'
-        colored.addEventListener("contextmenu",function() {return context(this)})
         tooltip(colored,'top','Choose me  !',true,'3rem','3.5rem')
         colored.addEventListener('click', () => {
             document.getElementById('pick-color').value=e
             printColor(e)
         });
-        ++i    
-    });
+        --i
+    })
 }
 // suprime l'historique
 export function clearHistory() {
     if (localStorage.getItem('colors')) localStorage.removeItem('colors')
     history()
-}
-// suprime une couleur dans l'historique
-export function clearColor() {
-    if (localStorage.getItem('colors')) {
-        const removeProduct = productId => {
-            let products = localStorage.getItem('colors')
-            const index = products.findIndex(product => product.product_id === productId)
-            if (index > -1) {
-                products.slice(index, 1)
-            }
-            localStorage.setItem('colors')
-        }
-    history()
-    }
 }
 // sauvegarde la couleur dans l'historique
 export function saveColor(color) {
