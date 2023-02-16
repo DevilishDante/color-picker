c77379aaf620c4e3d824324821e4ad2ad13a20e2
BuildTools <unconfigured@null.spigotmc.org>
2023-02-16 11:22:15 +0100

    v1.1

--- app/assets/js/core.js
+++ app/assets/js/core.js
@@ -8,4 +8,3 @@ svg('#00000');
 document.getElementById('pick-color').addEventListener('input',e  => printColor(e.target.value))
 document.getElementById('pick-color').addEventListener('change',e  => printColor(e.target.value))
 document.getElementById('save-color').addEventListener('click',() => saveColor(document.getElementById('pick-color').value))
-document.getElementById('clear-history').addEventListener('click',()  => clearHistory())
